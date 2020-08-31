e81563338f0fa83f01c28d0a8f2ca0a248eab7d9
Joe Tseng <jtsengorg@gmail.com>
2020-08-31 15:11:14 -0400

    1. Added roles table. 2. Inserted seed data for Roles and Users tables.

--- srv_users/migrations/alembic/versions/2badbf2cc72a_add_users_db.py
+++ srv_users/migrations/alembic/versions/2badbf2cc72a_add_users_db.py
@@ -6,8 +6,10 @@ Create Date: 2020-08-28 13:39:27.472806
 
 """
 from alembic import op
-from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func, Float
+from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
+import datetime
 
 
 # revision identifiers, used by Alembic.
@@ -19,27 +21,57 @@ depends_on = None
 
 def upgrade():
     op.create_table(
+        'roles',
+        Column('id', Integer, primary_key=True),
+        Column('name', String, nullable=False)
+    )
+
+    op.execute("INSERT INTO roles (name)"
+               "VALUES ('administrator'), ('editor'), ('employee'), ('customer')")
+
+    op.create_table(
         'states',
         Column('id', Integer, primary_key=True),
         Column('name', String, nullable=False),
-        Column('tax_rate', Integer, nullable=False),
+        Column('tax_rate', Float, nullable=False),
         Column('abbreviation', String)
     )
+
+    op.execute("INSERT INTO states (name, tax_rate, abbreviation)"
+               "VALUES ('Maryland', 6.0, 'MD'),"
+               "('Virginia', 6.0, 'VA'),"
+               "('District of Columbia', 6.0, 'DC')")
 
     op.create_table(
         'users',
         Column('id', Integer, primary_key=True),
-        Column('password_hash', String),
+        Column('password_hash', String, nullable=False),
         Column('uuid', String, default=uuid.uuid4()),
-        Column('name', String),
-        Column('address1', String),
+        Column('first_name', String, nullable=False),
+        Column('last_name', String, nullable=False),
+        Column('address1', String, nullable=False),
         Column('address2', String),
-        Column('city', String),
+        Column('city', String, nullable=False),
         Column('state_id', Integer, ForeignKey('states.id')),
-        Column('zip', Integer),
+        Column('zip', Integer, nullable=False),
         Column('added_on', DateTime, default=func.now()),
         Column('is_employee_account', DateTime),
         Column('is_disabled_account', DateTime)
+    )
+
+    op.execute(
+        "INSERT INTO users (password_hash, uuid, first_name, last_name, address1, city, state_id, zip, "
+        "added_on, is_employee_account) "
+        " VALUES "
+        "("
+            "'" + generate_password_hash("password") + "',"
+            "'" + str(uuid.uuid4()) + "', 'Admin', 'User', '1600 Pennsylvania Ave',"
+            "'Washington',"
+            "(select (id) from states where abbreviation = 'DC'),"
+            "20001,"
+            "datetime('now', 'localtime'),"
+            "datetime('now', 'localtime')"
+        ")"
     )
 
     op.create_table(
@@ -56,5 +88,6 @@ def upgrade():
 
 def downgrade():
     op.drop_table('payment_cards')
-    op.drop_table('user')
+    op.drop_table('users')
     op.drop_table('state')
+    op.drop_table('roles')
